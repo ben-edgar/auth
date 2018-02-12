6c68419c5ca955775319594d6d096ce048813238
Ben Edgar <ben.edgar@appian.com>
2018-02-12 09:56:48 -0500

    Logout button works now

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   renderContent = () => {
     switch(this.state.loggedIn) {
       case true:
-        return (<Card><CardSection><Button>Log Out</Button></CardSection></Card>);
+        return (<Card><CardSection><Button onPress={() => firebase.auth().signOut()}>Log Out</Button></CardSection></Card>);
       case false:
         return (<LoginForm />);
       default:
